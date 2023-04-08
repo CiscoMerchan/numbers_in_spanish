2ebbf582c89cca42968883f21dd32c64268aa16d
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-04-08 16:18:22 +1000

    add  animation to buttons when either click or presskey

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
+// Add and remove class animate_btn 
 $(document).ready(function(){
     $('button').click(function(){
-        // console.log(this)
-    //    this.addClass('animate_btn');
-    //    setTimeout(this.removeClass('animate_btn'), 1000)
-        
-      });
-})
+      var button = $(this); // store the reference to the button element. otherwise "this" will be window.
+      button.addClass('animate_btn');
+      setTimeout(function() {
+        button.removeClass('animate_btn'); // use the stored reference to remove the class
+      }, 200);
+    });
+  });
 
 
-
-// add event listeners keydown
+/* Event listeners keydown. When user presskey on the correspondant number it will
+activate the sound and animate the button*/  
 function press(){ 
     $(document).keydown(function(event){
      var num = event.key;
   
 switch(num){
 case'1':
-var uno = new Audio('assets/sounds/1.mp3')
-uno.play();
+playSound('assets/sounds/1.mp3')
+animateButton($('.b1'));
 break;
 case'2':
-var dos = new Audio('assets/sounds/2.mp3')
-dos.play();
+playSound('assets/sounds/2.mp3')
+animateButton($('.b2'));
 break;
 case'3':
-var tres = new Audio('assets/sounds/3.mp3')
-tres.play();
+playSound('assets/sounds/3.mp3')
+animateButton($('.b3'));
 break;
 case'4':
-var cuatro = new Audio('assets/sounds/4.mp3')
-cuatro.play();
+playSound('assets/sounds/4.mp3')
+animateButton($('.b4'));
 break;
 case'5':
-var cinco = new Audio('assets/sounds/5.mp3')
-cinco.play();
+playSound('assets/sounds/5.mp3')
+animateButton($('.b5'));
 break;
 case'6':
-var seis = new Audio('assets/sounds/6.mp3')
-seis.play();
+playSound('assets/sounds/6.mp3')
+animateButton($('.b6'));
 break;
 case'7':
-var siete = new Audio('assets/sounds/7.mp3')
-siete.play();
+playSound('assets/sounds/7.mp3')
+animateButton($('.b7'));
 break;
 case'8':
-var ocho = new Audio('assets/sounds/8.mp3')
-ocho.play();
+playSound('assets/sounds/8.mp3')
+animateButton($('.b8'));
 break;
 case'9':
-var nueve = new Audio('assets/sounds/9.mp3')
-nueve.play();
+playSound('assets/sounds/9.mp3')
+animateButton($('.b9'));
 break;
 case'0':
-var diez = new Audio('assets/sounds/10.mp3')
-diez.play();
+playSound('assets/sounds/10.mp3')
+animateButton($('.b10'));
 
 break;
 default:
@@ -62,15 +64,27 @@ default:
 }
 });
 }
+/* This function creates a new Audio object with the provided sound URL,
+ and then plays the sound using the play() method. Rather than rewrite in whole code 
+ in each case of the switch with this function I just invoke playSound and as 
+ parameter the soundUrl  */
 function playSound(soundUrl) {
     var sound = new Audio(soundUrl);
     sound.play();
     console.log(sound);
 
 }
+
+function animateButton(button) {
+    button.addClass('animate_btn');
+    setTimeout(function() {
+      button.removeClass('animate_btn');
+    }, 200);
+  }
+
 press();
 
-// add event listeners to buttons
+// Eevent listeners and animation to buttons when the user click on mouse
 $('.b1').click(function() {
     playSound('assets/sounds/1.mp3');
   });
